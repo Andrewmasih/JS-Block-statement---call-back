ab5e2a656e478370f5866330bd8ad82574431f60
Andrew Masih <armasih@outlook.com>
2021-05-04 22:50:59 +0000

    added text

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
-/* a call back function passed into another function as an arument, which is then invoked inside the outer function to complete somekind of routine or action */
+/* 'call backs' a function that gets passed to a function as an arument, it passes a functon as an argument, instead of a value it passes a function as a argument. 
+
+a call back functon passed ino another function as an argument, which then invoked inside the outer function to complete somekind of routine or action, here is an example  */
 
 function ask (question, good, bad) {
 
   /*we declare a function called 'ask', it takes 3 parameters, within the body of the 'ask' function, we have an 'if', 'else' code block */
 
 
- /* the 'confirm' function takes one argument, the text shown in the dialogue box */
+ /* the 'confirm' function takes one argument */
+ 
   if (confirm (question)) {
     good ();
   }
